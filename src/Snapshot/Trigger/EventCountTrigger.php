2de89b56017c48de7922847e9231c8106e2eb668
Artem Onyschenko <a.onyshchenko@cleack.com>
2024-04-18 01:49:31 +0300

    Update EventCountTrigger, update method shouldSnapshot

--- src/Snapshot/Trigger/EventCountTrigger.php
+++ src/Snapshot/Trigger/EventCountTrigger.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MicroModule\Snapshotting\Snapshot\Trigger;
 
+use Broadway\EventSourcing\ShouldNotStoredEvent;
 use MicroModule\Snapshotting\Snapshot\TriggerInterface;
 use Broadway\EventSourcing\EventSourcedAggregateRoot;
 
@@ -30,6 +31,9 @@ class EventCountTrigger implements TriggerInterface
         $clonedAggregateRoot = clone $aggregateRoot;
 
         foreach ($clonedAggregateRoot->getUncommittedEvents() as $domainMessage) {
+            if ($domainMessage->getPayload() instanceof ShouldNotStoredEvent) {
+                continue;
+            }
             if (0 === ($domainMessage->getPlayhead() + 1) % $this->eventCount) {
                 return true;
             }
